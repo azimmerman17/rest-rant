65a4c40a59f0ef311d5c2c8bd6efde79218d97ee
Andrew Zimmerman <azimmerman@pga.com>
2022-11-26 13:22:35 -0500

    Added 404 page route

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
     res.send('hw')
 })
 
+// 404 not found
+app.get('*', (req, res) => {
+    res.status(404).send(
+        '<h1>404 - Not Found</h1>'
+    )
+})
+
 const PORT = process.env.PORT
 
 app.listen(PORT, console.log(`listen-${PORT}`))
