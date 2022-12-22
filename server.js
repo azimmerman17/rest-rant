c9823eb204c61d7b13fd7ff1a47563e486a0287a
Andrew Zimmerman <azimmerman@pga.com>
2022-12-22 09:07:06 -0500

    added export to server.js

--- server.js
+++ server.js
@@ -42,3 +42,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const PORT = process.env.PORT
 
 app.listen(PORT, console.log(`listen-${PORT}`))
+
+module.exports = app
